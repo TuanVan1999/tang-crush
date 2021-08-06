88b9fedeedb142b88baeec4dca0cf9988a4c35dc
TuanVan1999 <88308569+TuanVan1999@users.noreply.github.com>
2021-08-06 17:17:06 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -96,7 +96,7 @@ function textGenerate() {
 
 // show popup
 $('#yes').click(function() {
-    var audio = new Audio('sound/tick.mp3');
+    var audio = new Audio('sound/LS.mp3');
     audio.play();
     Swal.fire({
         title: CONFIG.question,
